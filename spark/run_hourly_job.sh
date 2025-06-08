d227866774d29feb410ad1bb2f287c8ae4e13f44
Ala Eddine Achach <122971085+Ala-Eddine-Achach@users.noreply.github.com>
2025-06-08 17:13:43 +0100

    Fix Spark MongoDB Connector configuration keys

--- spark/run_hourly_job.sh
+++ spark/run_hourly_job.sh
@@ -5,8 +5,8 @@ while true; do
   /opt/bitnami/spark/bin/spark-submit \
     --master local[*] \
     --packages org.mongodb.spark:mongo-spark-connector_2.12:10.5.0 \
-    --conf "spark.mongodb.input.uri=${MONGO_URL}github.raw_prs" \
-    --conf "spark.mongodb.output.uri=${MONGO_URL}github.analytics" \
+    --conf "spark.mongodb.read.connection.uri=mongodb://mongodb:27017/github.raw_prs" \
+    --conf "spark.mongodb.write.connection.uri=mongodb://mongodb:27017/github.analytics" \
     /app/spark/spark-batch-job.py
   echo "Spark batch job finished. Sleeping for 1 hour..."
   sleep 60 # Sleep for 1 hour (3600 seconds)
